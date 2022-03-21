2ef80c66b6270c4113b9e730dbce348ddea516d2
xiomara gutierrez <190040@mail.unacar.mx>
2022-03-20 22:26:07 -0600

    int2

---
+++ consonante.js
@@ -0,0 +1,9 @@
+var letra = prompt("escribe una letra");
+const vocales = ["a", "e", "i", "o", "u","A", "E", "I", "O", "U"];
+const consonante = ["b", "c", "d","f","g","h","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","B","C","D","F","G","H","J","K","L","M","N","Ñ","P","Q","R","R","T","V","W","X","Y","Z",];
+if(letra== vocales.indexOf){
+    alert("VOCAL");
+}
+else if  (letra== consonante.indexOf){
+    alert("consonante");
+}
